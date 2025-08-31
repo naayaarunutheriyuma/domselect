82c23a1e884a27ab49b5cc32f13c9a0089cdf3f7
User <guest@example.com>
2025-08-31 13:58:22 +0700

    Bump version: 0.0.1 → 0.0.2

--- domselect/__init__.py
+++ domselect/__init__.py
@@ -3,4 +3,4 @@ from .lexbor_selector import LexborSelector
 from .lxml_selector import LxmlSelector
 
 __all__ = ["BaseSelector", "LexborSelector", "LxmlSelector"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
